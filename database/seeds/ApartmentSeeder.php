036093f1c39a07302cb8ae32e2cd2f609aa960a1
ItzArima <skaypaprilia@gmail.com>
2022-02-10 19:24:23 +0100

    apartment seeder added

--- database/seeds/ApartmentSeeder.php
+++ database/seeds/ApartmentSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Apartment;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class ApartmentSeeder extends Seeder
 {
@@ -11,6 +13,43 @@ class ApartmentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $samples = [
+            [
+                'title' => 'Cantami o Diva del pelide Achille l\'ira funesta',
+                'address' => 'via Del Pelide Achille 83 04011 Aprilia LT',
+                'latitude' => 13.89649743,
+                'longitude' => 111.23413764,
+                'number_of_rooms' => 3,
+                'number_of_beds' => 5,
+                'number_of_baths' => 1,
+                'square_metres' => 33,
+                'slug' => Str::slug('Cantami o Diva del pelide Achille l\'ira funesta')
+            ],
+            [
+                'title' => 'Cantami o Diva del pelide Achille l\'ira funesta',
+                'address' => 'via Enzo Dotti 11 00040 Roma RM',
+                'latitude' => 14.86249743,
+                'longitude' => 13.11413664,
+                'number_of_rooms' => 6,
+                'number_of_beds' => 1,
+                'number_of_baths' => 5,
+                'square_metres' => 89,
+                'slug' => Str::slug('Cantami o Diva del pelide Achille l\'ira funesta')
+            ]
+        ];
+
+        foreach($samples as $sample){
+            $apartment = new Apartment();
+            $apartment->title = $sample['title'];
+            $apartment->address = $sample['address'];
+            $apartment->latitude = $sample['latitude'];
+            $apartment->longitude = $sample['longitude'];
+            $apartment->number_of_rooms = $sample['number_of_rooms'];
+            $apartment->number_of_beds = $sample['number_of_beds'];
+            $apartment->number_of_baths = $sample['number_of_baths'];
+            $apartment->square_metres = $sample['square_metres'];
+            $apartment->slug = $sample['slug'];
+            $apartment->save();
+        }
     }
 }
