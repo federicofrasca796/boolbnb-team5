e84561c3cc1d5cc5924a0769ad3e79362cd83399
Valerio Corda <corda.valerio05@GMAIL.COM>
2022-02-10 21:04:01 +0100

    change the users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 25);
+            $table->string('slug');
+            $table->string('surname', 25);
+            $table->date('date_of_birth')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
