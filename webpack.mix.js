0a0cf27c31502dfd87732c67cbac81a94d712834
Federico Frasca <federico.frasca796@gmail.com>
2022-02-10 16:31:36 +0100

    add img,ura,guest folders, update routes
    add sample views, compile resources folder

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .options({
+        processCssUrls: false
+    })
+    .copyDirectory('resources/img', 'public/img')
+    .sass('resources/sass/ura.scss', 'public/css')
     .sass('resources/sass/app.scss', 'public/css');
