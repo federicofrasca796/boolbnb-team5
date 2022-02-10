03babc232f891324c690434da2c29c9f4d6fedc1
Valerio Corda <corda.valerio05@GMAIL.COM>
2022-02-10 17:56:51 +0100

    populate the database via the seeder using dummy data

--- database/seeds/MessageSeeder.php
+++ database/seeds/MessageSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Message;
 use Illuminate\Database\Seeder;
 
 class MessageSeeder extends Seeder
@@ -12,5 +13,31 @@ class MessageSeeder extends Seeder
     public function run()
     {
         //
+        $messages = [
+            [
+                'name'=>'Valerio Corda',
+                'mail'=>'pinkopallino@example.com',
+                'body'=> 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam corporis recusandae iste eaque, doloribus nostrum mollitia eius sint adipisci nobis aliquid odit optio sequi pariatur? Officia facilis magni tenetur?'
+            ],
+            [
+                'name'=>'Chandra Rota',
+                'mail'=>'chandra@example.com',
+                'body'=> 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam corporis recusandae iste eaque, doloribus nostrum mollitia eius sint adipisci nobis aliquid odit optio sequi pariatur? Officia facilis magni tenetur?'
+            ],
+            [
+                'name'=>'Federico Frascà',
+                'mail'=>'graphicDesigner@example.com',
+                'body'=> 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quisquam corporis recusandae iste eaque, doloribus nostrum mollitia eius sint adipisci nobis aliquid odit optio sequi pariatur? Officia facilis magni tenetur?'
+            ],
+            
+        ];
+
+        foreach ($messages as $message) {
+            $_message = new Message();
+            $_message->name = $message['name'];
+            $_message->mail = $message['mail'];
+            $_message->body = $message['body'];
+            $_message->save();
+        }
     }
 }
