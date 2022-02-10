6383de4fc300d242da928909a89e05d740fd9c13
Federico Frasca <federico.frasca796@gmail.com>
2022-02-10 17:58:43 +0100

    complete service migration and seeder

--- database/migrations/2022_02_10_162153_create_services_table.php
+++ database/migrations/2022_02_10_162153_create_services_table.php
@@ -15,6 +15,8 @@ class CreateServicesTable extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 25);
+            $table->string('slug');
             $table->timestamps();
         });
     }
--- database/seeds/ServiceSeeder.php
+++ database/seeds/ServiceSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Service;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class ServiceSeeder extends Seeder
 {
@@ -11,6 +13,13 @@ class ServiceSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $services = ['WiFi', 'Kitchen', 'Self check-in', 'Pool', 'Hot tub', 'Free parking', 'Air Conditioning', 'Gym', 'EV charger', 'Smoke alarm', 'Fireplace', 'Dryer', 'Washer', 'Dedicated workspace', ''];
+
+        foreach ($services as $service) {
+            $newService = new Service;
+            $newService->name = $service;
+            $newService->slug = Str::slug($service, '-');
+            $newService->save();
+        }
     }
 }
