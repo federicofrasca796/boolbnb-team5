1fbd9463dc5ddb61e2e14c2636bf60690a3e08e3
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-02-11 12:18:21 +0100

    fix comment DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(SponsorSeeder::class);
+        //$this->call(SponsorSeeder::class);
 
     }
 }
