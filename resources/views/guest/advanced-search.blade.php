13f5b39a25be3fceb501be87f44f5a38b1a92d4c
rota99C <rotaboolean@gmail.com>
2022-02-18 21:51:00 +0100

    wroking in progress

--- resources/views/guest/advanced-search.blade.php
+++ resources/views/guest/advanced-search.blade.php
@@ -6,36 +6,39 @@
     @endforeach
 </div>
 <div class="container_results_appartment d-flex px-4 py-3">
-    <div class="col-12 col-md-6 bg-primary">
+    <div class="col-12 col-md-6">
         <div class="number_results">
             <p>Roma: oltre 3000 appartamenti</p>
         </div>
         @foreach($apartments as $apartment)
-        <div class=" single-apartment d-flex py-3">
+        <div class=" single-apartment d-flex flex-wrap py-3">
             <div class="image-single h-100 overflow-hidden col-12 col-md-4">
                 <a href=" class=" w-100">
                     <img src="{{asset('storage/' . $apartment->thumbnail)}}" class="w-100 " alt="...">
                 </a>
 
             </div>
-            <div class="col-12 col-md-8">
-                <p>{{$apartment->address}}</p>
-                <h5>{{$apartment->title}}</h5>
-                <p><span>{{$apartment->square_metres}} m&sup2;-</span>
-                    <span>
-                        {{$apartment->number_of_rooms}} rooms-
-                    </span>
-                    <span>
-                        {{$apartment->number_of_beds}} beds-
-                    </span>
-                    <span>
-                        {{$apartment->number_of_baths}} baths
-                    </span>
-                </p>
-                @foreach ($apartment->services as $service)
-                <p><span>{{$service->name}}-</span>
-                </p>
-                @endforeach
+            <div class="info_apartment col-12 col-md-8 ps-4 d-flex align-items-center">
+                <div>
+                    <p>{{$apartment->address}}</p>
+                    <h5>{{$apartment->title}}</h5>
+                    <hr>
+                    <p class="m-0"><span>{{$apartment->square_metres}} m&sup2;-</span>
+                        <span>
+                            {{$apartment->number_of_rooms}} rooms-
+                        </span>
+                        <span>
+                            {{$apartment->number_of_beds}} beds-
+                        </span>
+                        <span>
+                            {{$apartment->number_of_baths}} baths
+                        </span>
+                    </p>
+                    @foreach ($apartment->services as $service)
+                    <span>{{$service->name}}-</span>
+                    @endforeach
+                </div>
+
 
 
 
@@ -44,7 +47,11 @@
         </div>
         @endforeach
     </div>
-    <div></div>
+    <div class=" mappa col-12 col-md-6">
+        <img src="{{asset('storage/apartments_img/map_adavanced.png')}}" class="w-100 " alt="...">
+
+
+    </div>
 
 </div>
 
