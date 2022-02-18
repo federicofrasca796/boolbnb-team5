fe2c50a2cba508530dfb15ce163ef8ef8066623b
rota99C <rotaboolean@gmail.com>
2022-02-18 19:38:30 +0100

    advanced search in progress

--- resources/views/guest/advanced-search.blade.php
+++ resources/views/guest/advanced-search.blade.php
@@ -4,9 +4,48 @@
     @foreach($services as $service)
     <input type="button" class="rounded-pill" value="{{$service->name}}"></input>
     @endforeach
-    <input type="button" class="rounded-pill" value="More filters">
+</div>
+<div class="container_results_appartment d-flex px-4 py-3">
+    <div class="col-12 col-md-6 bg-primary">
+        <div class="number_results">
+            <p>Roma: oltre 3000 appartamenti</p>
+        </div>
+        @foreach($apartments as $apartment)
+        <div class=" single-apartment d-flex py-3">
+            <div class="image-single h-100 overflow-hidden col-12 col-md-4">
+                <a href=" class=" w-100">
+                    <img src="{{asset('storage/' . $apartment->thumbnail)}}" class="w-100 " alt="...">
+                </a>
 
-    </input>
+            </div>
+            <div class="col-12 col-md-8">
+                <p>{{$apartment->address}}</p>
+                <h5>{{$apartment->title}}</h5>
+                <p><span>{{$apartment->square_metres}} m&sup2;-</span>
+                    <span>
+                        {{$apartment->number_of_rooms}} rooms-
+                    </span>
+                    <span>
+                        {{$apartment->number_of_beds}} beds-
+                    </span>
+                    <span>
+                        {{$apartment->number_of_baths}} baths
+                    </span>
+                </p>
+                @foreach ($apartment->services as $service)
+                <p><span>{{$service->name}}-</span>
+                </p>
+                @endforeach
+
+
+
+            </div>
+
+        </div>
+        @endforeach
+    </div>
+    <div></div>
+
 </div>
 
 
