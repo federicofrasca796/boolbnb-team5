4bb39ebea83d7d2951e03e01a91feb8a3d9f2bb2
Federico Frasca <federico.frasca796@gmail.com>
2022-02-18 10:54:46 +0100

    add api resource class | change api controller
    collection resource for pagination | api apt resource controller

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -5,10 +5,84 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Apartment;
 use Illuminate\Http\Request;
+use App\Http\Resources\ApartmentResource;
 
 class ApartmentController extends Controller
 {
-    public function index(){
-        return Apartment::all();
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // return Apartment::paginate(10);
+        return new ApartmentResource(Apartment::paginate(10));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Apartment  $apartment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Apartment $apartment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Apartment  $apartment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Apartment $apartment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Apartment  $apartment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Apartment $apartment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Apartment  $apartment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Apartment $apartment)
+    {
+        //
     }
 }
