9c0342e67bd0d43ca22ec2fd987508e754084234
Federico Frasca <federico.frasca796@gmail.com>
2022-02-21 15:38:03 +0100

    delete duplicates pt.3

--- resources/views/ura/apartments/show.blade 2.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.ura')
-
-@section('content')
-
-    <h1>Singolo appartamento</h1>
-    <p>{{ $apartment->title }}</p>
-    <img src="{{ asset('storage/' . $apartment->thumbnail) }}" alt="{{ $apartment->title }}">
-
-@endsection
