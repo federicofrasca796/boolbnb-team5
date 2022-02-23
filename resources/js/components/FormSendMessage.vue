6013d691c6aec5d7c8806071b524cd6e3c255fbf
Valerio Corda <corda.valerio05@GMAIL.COM>
2022-02-23 08:03:15 +0100

    prepare the form and add the v-model to control the inputs

--- resources/js/components/FormSendMessage.vue
+++ resources/js/components/FormSendMessage.vue
@@ -2,19 +2,16 @@
   <div id="form">
       <form action="" method="post">
           <div class="mb-3">
-            <label for="" class="form-label"></label>
-            <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
-            <small id="helpId" class="text-muted">Help text</small>
+            <label for="name" class="form-label">Name</label>
+            <input type="text" name="name" id="name" class="form-control" placeholder="Type your name" v-model="fields.name">
           </div>
           <div class="mb-3">
-            <label for="" class="form-label"></label>
-            <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
-            <small id="helpId" class="text-muted">Help text</small>
+            <label for="email" class="form-label"></label>
+            <input type="text" name="email" id="email" class="form-control" placeholder="type your email" v-model="fields.email">
           </div>
           <div class="mb-3">
-            <label for="" class="form-label"></label>
-            <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
-            <small id="helpId" class="text-muted">Help text</small>
+            <label for="body" class="form-label">Message</label>
+            <input type="text" name="body" id="body" class="form-control" placeholder="type your message" v-model="fields.body">
           </div>
       </form>
   </div>
@@ -22,7 +19,13 @@
 
 <script>
 export default {
-
+    data(){
+        return{
+            fields:{},
+        }
+    },
+    methods: {
+  },
 }
 </script>
 
