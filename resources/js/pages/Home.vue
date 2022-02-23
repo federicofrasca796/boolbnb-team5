57e15e17b41d7bc22dceb0d5c1744b727372bd99
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-02-23 21:39:26 +0100

    implement page Home.vue with sponsored apartments

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -36,7 +36,42 @@
       </div>
     </section>
 
+    <!-- Sponsored Apartments -->
+      <section>
+          <div class="sponsor_title my-5 text-center">
+         <h1 v-show="visible">Apartments in evidence</h1>
+      </div>
+
+     <!-- Apartments cards -->
+      <div class="container-apartments row m-auto row-cols-1 row-cols-md-4 g-4">
+        <template v-if="api_error">Apartments can't be reached</template>
+
+        <!-- While loading API.. -->
+        <template v-else-if="loading"> ⏳ Loading.. </template>
+
+        <!-- API loaded -->
+        <template v-else>
+
+          <div class="col" v-for="apartment in apartments_sponsored" :key="apartment.id">
+            <div class="card overflow-hidden">
+              <router-link :to="'/apartments/' + apartment.slug">
+                <img
+                  :src="'storage/' + apartment.thumbnail"
+                  class="w-100"
+                  :alt="apartment.slug"
+                />
+                <div class="card-body">
+                  <h5 class="card-title">{{ apartment.title }}</h5>
+                </div>
+              </router-link>
+            </div>
+          </div>
+        </template>
+      </div>
+    </section>
+
     <section>
+
       <div class="sponsor_title my-5 text-center">
         <h1>Explore our best apartments</h1>
       </div>
@@ -78,11 +113,14 @@ export default {
       loading: true,
       api_error: false,
       mySearchResult: Object,
-      inputValue : null 
+      inputValue : null,
+      apartments_sponsored: Array,
+      visible: false
     };
   },
   mounted() {
     this.fetchApartments();
+    this.getSponsored();
 
     //Searchbar
     var options = {
@@ -138,6 +176,24 @@ export default {
 
     emitSearchData(){
       this.$router.push({name:"Search" , params:{data: this.mySearchResult , value: this.inputValue , apartments: this.apartments}})
+    },
+    getSponsored() {
+      axios
+        .get("/api/sponsored")
+        .then(r => {
+            if (r.data.data == "") {
+                this.visible = false;
+            } else {
+                console.log(r);
+                this.apartments_sponsored = r.data.data;
+                this.loading = false;
+                this.visible = true;
+            }
+        })
+        .catch((e) => {
+          console.error(e);
+          this.api_error = true;
+        });
     }
   },
 };
