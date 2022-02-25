764b082e24c7253a84d6a2fd67130c2a95a5d16a
Valerio Corda <corda.valerio05@GMAIL.COM>
2022-02-25 11:14:11 +0100

    resolve conflict MessageController

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -10,6 +10,15 @@ use Illuminate\Support\Facades\Mail;
 
 class MessageController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
 
     /**
      * Show the form for creating a new resource.
@@ -18,6 +27,7 @@ class MessageController extends Controller
      */
     public function create(Apartment $apartment)
     {
+        
         return view('guest.formMessage', compact('apartment'));
     }
 
@@ -29,6 +39,7 @@ class MessageController extends Controller
      */
     public function store(Request $request)
     {
+    
         $validated_Data = $request->validate([
             'name' => 'required|min:3|max:80',
             'mail' => 'required',
@@ -36,15 +47,57 @@ class MessageController extends Controller
             'apartment_id' => 'required|exists:apartments,id',
             'user_id' => 'nullable|exists:users,id',
         ]);
-
         $message = Message::create($validated_Data);
 
-        /* Add a render email */
-        /* return (new InfoApartmentMail($message))->render(); */
+        //return (new InfoApartmentMail($message))->render();
 
         Mail::to('boolbnb@admin.com')->send(new InfoApartmentMail($message));
-
         return redirect()->back()->with(session()->flash('success', "Message send succesfully"));
 
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Message $message)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Message $message)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Message $message)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Message $message)
+    {
+        //
     }
 }
