55f8f330f77d121a5636587f8a68c3b20649dca7
Valerio Corda <corda.valerio05@GMAIL.COM>
2022-02-28 16:20:57 +0100

    remove requirede register form backend

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,8 +51,8 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:25'],
-            'surname' => ['required', 'string', 'max:25'],
+            'name' => ['string', 'max:25'],
+            'surname' => ['string', 'max:25'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'date_of_birth' => ['nullable', 'date', 'before:today', 'after:1900-01-01', 'before_or_equal:' . now()->subYears(18)->toDateString()],
