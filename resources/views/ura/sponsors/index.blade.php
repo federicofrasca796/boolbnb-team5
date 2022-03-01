0193f7d4c439eaea351e45a92d7e411a048510ec
Federico Frasca <federico.frasca796@gmail.com>
2022-03-01 12:59:42 +0100

    fix sponsor index table

--- resources/views/ura/sponsors/index.blade.php
+++ resources/views/ura/sponsors/index.blade.php
@@ -10,38 +10,30 @@
         </div>
     </div>
     <div class="container_ura_index my-5  p-2 p-md-0 m-auto">
-        <div class="table w-100">
+        <div class="table table-striped w-100">
             <div class="thead w-100">
                 <div class="table-row w-100 d-flex">
-                    <div class="table-cell col-1 d-none d-md-block">Apartment</div>
+                    <div class="table-cell col-1 d-md-block">Apartment</div>
                     <div class="table-cell col-2 d-none d-md-block">Sponsorship Plan</div>
-                    <div class="table-cell col-md-3 col-5">Expires on</div>
+                    <div class="table-cell col-md-3 col-6 text-md-start text-end">Expires on</div>
                 </div>
             </div>
-            @forelse ($apartment_sponsored as $apartment)
-                @foreach ($apartment->sponsors as $sponsor)
-                    <div class="tbody">
-                        <div class="table-row w-100 d-flex {{ $loop->odd ? 'bg-lightgrey' : '' }}">
-                            <div class="table-cell col-1 d-none d-md-block">{{ $apartment->title }}</div>
+            <div class="tbody w-100">
+                @forelse ($apartment_sponsored as $apartment)
+                    <div class="table-row w-100 d-flex {{ $loop->odd ? 'bg-lightgrey' : '' }}">
+                        @foreach ($apartment->sponsors as $sponsor)
+                            <div class="table-cell col-1 d-md-block">{{ $apartment->title }}</div>
                             <div class="table-cell col-2 d-none d-md-block">{{ $sponsor->name }}</div>
-                            <div class="table-cell col-md-3 col-5">
+                            <div class="table-cell col-md-3 col-6 text-md-start text-end">
                                 {{ $sponsor->pivot->expires_on }}
                             </div>
-                        </div>
+                        @endforeach
                     </div>
-                @endforeach
+                @empty
+                    <h2 class="text-center">Sorry, you have no sponsorships yet.</h2>
+                @endforelse
+            </div>
         </div>
-    @empty
-        <h2 class="text-center">Sorry, you have no sponsorships yet.</h2>
-        @endforelse
     </div>
-
-
-
-
-
-
-
-
 
 @endsection
